d590f66d3d537f725ff818c883439235b36e97a1
minZzy <120133624+myminzy@users.noreply.github.com>
2025-06-23 14:02:07 +0900

    Delete web4_3일차 directory

--- web4_3일차/web4/js/index1.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $(".javascript").css({"color":"#f90"});
-   // $(".css").css({"margin-left":"50px"});
-    //$(".css").css({"margin-left":100});
-    $(".css").css({marginLeft:150});
-});
